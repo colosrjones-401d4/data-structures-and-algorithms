c6a868e0087670e2f85fa434e4cc3fbd2fa0b40b
ColoSRJones <colosrjones@gmail.com>
2019-09-27 13:12:25 -0500

    commit to new branch

--- code-challenges/401/stacksAndQueues/_test_/stacks-and-queues.test.js
+++ code-challenges/401/stacksAndQueues/_test_/stacks-and-queues.test.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const SandQ = require('../../stacks-and-queues');
+const SandQ = require('../stacks-and-queues');
 
 describe('Test for Stacks and Queues', () => {
-
+// First test passed!!!
   it('Can successfully push onto a stack', () => {
     const stackTest = new SandQ.Stack();
     stackTest.push(3);
