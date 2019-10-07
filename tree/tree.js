18522998da93e8c34a7dff503fc38374e5cd1091
ColoSRJones <colosrjones@gmail.com>
2019-10-07 08:25:39 -0500

    updated tree

--- tree/tree.js
+++ tree/tree.js
@@ -1,5 +1,4 @@
 'use strict';
-
 class Node {
   constructor(value) {
     this.value = value;
@@ -125,5 +124,5 @@ module.exports = {
   BinaryTree,
   preOrder,
   postOrder,
-  inOrder
+  inOrder,
 };
